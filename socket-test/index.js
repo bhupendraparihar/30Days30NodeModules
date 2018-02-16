e3e17e909ea37dc9d4ed8f56679c1880abe99c5f
Bhupendra Singh Parihar <bhupendraparihar@outlook.com>
2018-02-16 12:34:48 +0530

    socket-test commit

---
+++ socket-test/index.js
@@ -0,0 +1,32 @@
+/**
+ * Socket io is used to send data from server to client
+ * npm install socket.io --save
+ * node index.js
+ * open http://localhost:3000 in two browsers and enjoy the chat
+ */
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.use(express.static(__dirname + '/node_modules/socket.io-client/dist'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+})
